40289ae29de892a44621e9e6d7ce38786de23cdb
Dan Rosén <danr@student.chalmers.se>
2012-11-12 11:46:07 +0100

    Remove an incorrect conversion from XML to JSON in progress bar updating

--- frontend/progress.js
+++ frontend/progress.js
@@ -10,27 +10,28 @@ function clear_progress_bar() {
 }
 
 function handle_progress(data) {
-    var footer = '</incremental></result>'
     var finished = data.indexOf('</result>') != -1;
-    if (!finished) {
-        data = data + footer;
-    }
-    var json = $.xml2json(data);
-    var progress = 100;
-    var command = ""
-    var i = json.incremental;
-    if (i && !finished) {
-        var steps = i.steps;
-        var step = 0;
-        if (i.increment) {
-            var ilen = i.increment.length;
-            if (ilen > 0) {
-                step = ilen;
-                command = i.increment[ilen-1].command;
+    if (finished) {
+        clear_progress_bar();
+    } else {
+        data = data + '</incremental></result>';
+        var json = $.xml2json(data);
+        var progress = 100;
+        var command = ""
+        var i = json.incremental;
+        if (i) {
+            var steps = i.steps;
+            var step = 0;
+            if (i.increment) {
+                var ilen = i.increment.length;
+                if (ilen > 0) {
+                    step = ilen;
+                    command = i.increment[ilen-1].command;
+                }
             }
+            progress = (steps > 0) ? (step / steps * 100) : 100;
+            $('#progress-text').text("Kör kommando " + command + "...");
         }
-        progress = step / steps * 100;
-        $('#progress-text').text("Kör kommando " + command + "...");
+        $('#progress-bar').css("width",progress + '%');
     }
-    $('#progress-bar').css("width",progress + '%');
 }
