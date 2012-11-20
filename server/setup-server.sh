f28057cd59192b8e8aaf47594b95f57f91f8a890
Dan Rosén <danr@student.chalmers.se>
2012-11-20 15:23:23 +0100

    Force update of Makefiles in setup server

--- server/setup-server.sh
+++ server/setup-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-svn export https://svn.spraakdata.gu.se/sb-arkiv/material/Makefile.rules
-svn export https://svn.spraakdata.gu.se/sb-arkiv/material/Makefile.common
+svn export https://svn.spraakdata.gu.se/sb-arkiv/material/Makefile.rules --force
+svn export https://svn.spraakdata.gu.se/sb-arkiv/material/Makefile.common --force
 
 make
